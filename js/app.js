e8fcc96d16ff03ba974faff414362066629e0888
Irini <742142833@qq.com>
2016-07-07 16:29:22 +0800

    ui-bootstrap

--- js/app.js
+++ js/app.js
@@ -1,21 +1,24 @@
- var app = angular.module('app', ['ui.router']);
- app.config(['$stateProvider',function($stateProvider) {
-/*    app.controller = $controllerProvider.register;
+ var app = angular.module('app', ['ui.router','ui.bootstrap']);
+ app.config(['$controllerProvider','$compileProvider','$filterProvider','$provide','$stateProvider',function($controllerProvider,$compileProvider,$filterProvider,$provide,$stateProvider) {
+    app.controller = $controllerProvider.register;
     app.directive = $compileProvider.directive;
     app.filter = $filterProvider.register;
     app.factory = $provide.factory;
     app.service = $provide.service;
     app.constant = $provide.constant;
-    app.value = $provide.value;*/
+    app.value = $provide.value;
     $stateProvider
     .state('index',{
     	url:'/index',
     	abstract:true,
     	templateUrl:'view/app.html',
-    	controller:'appCtrl'
+    	
     }).state('index.list',{
     	url:'/list',
-    	templateUrl:'view/app.html'
+    	templateUrl:'view/list.html'
+    }).state('index.list2',{
+    	url:'/list2',
+    	templateUrl:'view/list2.html'
     })
 
 
@@ -23,5 +26,5 @@
 
  }]);
  app.run(['$state','$rootScope','$stateParams',function($state,$rootScope,$stateParams) {
-         $state.go('index.list');
+         
 }]);
